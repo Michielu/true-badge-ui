dd5c3afd03b4ebd0b26945b44f19648bcb40c236
Michielu Menning <mich.menning@gmail.com>
2020-01-03 21:23:11 -0600

    Inital routing pages

---
+++ src/components/About/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div className="About">
+            <header className="App-header">
+
+
+            </header>
+            <body>
+                <p>
+                    About page
+        </p>
+            </body>
+        </div>
+    );
+}
+
+export default About;
