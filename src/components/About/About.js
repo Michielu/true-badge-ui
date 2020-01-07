80e1bad871f2ac5c29ba2612e81cf0926b28b46a
Michielu Menning <mich.menning@gmail.com>
2020-01-06 20:46:15 -0600

    SImple consolidation

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,15 +3,9 @@ import React from 'react';
 function About() {
     return (
         <div className="About">
-            <header className="App-header">
-
-
-            </header>
-            <body>
-                <p>
-                    About page
-        </p>
-            </body>
+            <p>
+                About page
+            </p>
         </div>
     );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,21 +2,8 @@ import React from 'react';
 
 function Home() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                    Hellow
-        </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-        </a>
-            </header>
+        <div>
+            <p>Home</p>
         </div>
     );
 }
