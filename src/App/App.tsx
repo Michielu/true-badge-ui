1d2e728522350dc85f268c262e2fc0e395e26afe
Michielu Menning <mich.menning@gmail.com>
2020-01-26 16:29:20 -0600

    Create Bootstrap Nav wrapper

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 
+import TbNav from "../partials/TbNav/TbNav";
 import Routing from '../utils/routing';
-import { Navbar, Nav } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/">True Badge</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/">Login/Signup</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <TbNav />
       <div className="app-body">
         {/* 
         <button> <Link to="/count">Test component</Link></button>
---
+++ src/partials/TbNav/TbNav.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Navbar, Nav } from 'react-bootstrap';
+
+interface NavProps {
+}
+
+const TbNav: React.FunctionComponent<NavProps> = (props) => {
+    return (
+        <div className="row tb-quote">
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="/">True Badge</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href="/">Login/Signup</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
+};
+export default TbNav;
