0b82b374e41bfbe278286b60148b0b1738fe20d6
Michielu Menning <mich.menning@gmail.com>
2020-01-26 16:03:55 -0600

    Initial home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-
-
+import {
+    Link
+} from "react-router-dom";
+import { Card } from 'react-bootstrap';
 
 const Home: React.FunctionComponent<{}> = () => {
     return (
         <div>
-            <p>Welcome to home Home</p>
+
+
+            <Card body className="tb-quote">
+                <i>“A person’s name is to him or her the sweetest and most important sound in any language.”</i>
+                – Dale Carnegie.
+                    </Card>
+
+
+            <br />
+            <button> <Link to="/create">Create Badge</Link></button>
+
         </div>
     )
 };
