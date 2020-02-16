97567b3d2902781d5fbea3b7473f673bdf15aad4
Michielu Menning <mich.menning@gmail.com>
2020-02-15 21:27:28 -0600

    Centralize partial exports

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import TbCard from '../../partials/TbCard/TbCard';
-import TbJumbotron from '../../partials/TbJumbotron/TbJumbotron';
+import { TbCard, TbJumbotron } from '../../partials/';
 
 
 const About: React.FunctionComponent<{}> = () => {
     const quote: string = "Names are a wonderful thing. A person’s name is the greatest connection to their own identity. Being able to pronounce someone’s name correctly, -- as the person themselves wants it pronounced – is a sign of courtesy, respect, and care.";
     const quote2: string = "We help you bridge the gap between face to names. We bundle your name, face, and how YOU pronounce your name onto one portable web page -- which you are given as a link -- so that you can share it with everyone.";
-    const pageTitle = "About";
+    const pageTitle: string = "About";
 
     return (
         <div>
---
+++ src/partials/index.tsx
@@ -0,0 +1,8 @@
+import TbButton from "./TbButton/TbButton";
+import TbCard from "./TbCard/TbCard";
+import TbJumbotron from "./TbJumbotron/TbJumbotron"
+import TbNav from "./TbNav/TbNav";
+
+export {
+    TbButton, TbCard, TbJumbotron, TbNav
+}
