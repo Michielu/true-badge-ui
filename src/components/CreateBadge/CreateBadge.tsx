73a6463a34b8fb949e0e613b80f882c22a165ea6
Michielu Menning <mich.menning@gmail.com>
2020-02-16 21:15:52 -0600

    Clean CreateBadge page to use TbForm

--- src/components/CreateBadge/CreateBadge.tsx
+++ src/components/CreateBadge/CreateBadge.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ImageUploader from 'react-images-upload';
 
+import { TbForm } from "../../partials";
 
 // function CreateBadge() {
 //     return (
@@ -19,43 +19,14 @@ interface State {
 };
 
 class CreateBadge extends React.Component<Props, State> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            picture: null,
-            uploadStatus: "Upload Image"
-        };
-        this.onDrop = this.onDrop.bind(this);
-    }
 
-    onDrop(pic) {
-        console.log("Pic: ", pic)
-        this.setState({
-            picture: pic,
-            uploadStatus: "Image successfully uploaded"
-        });
-    }
 
     render() {
         return (
             <div>
-                <form>
-                    <label>
-                        Name:
-                        <input type="text" name="name" />
-                    </label>
+                <TbForm>
 
-                    <input type="submit" value="Submit" />
-                </form>
-                <p>{this.state.uploadStatus}</p>
-                <ImageUploader
-                    withIcon={true}
-                    buttonText='Upload image'
-                    onChange={this.onDrop}
-                    imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                    maxFileSize={5242880}
-                    singleImage={true}
-                />
+                </TbForm>
 
             </div>
         );
