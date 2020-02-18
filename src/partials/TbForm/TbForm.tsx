0985e3d80e62b55d2e6b8fbe49eac5cf1026a5d8
Michielu Menning <mich.menning@gmail.com>
2020-02-17 19:50:07 -0600

    Clean TbForm

--- src/partials/TbForm/TbForm.tsx
+++ src/partials/TbForm/TbForm.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import { Button, Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
-import Dropzone, { useDropzone } from 'react-dropzone'
+import Dropzone from 'react-dropzone'
 
 // import { TbButton } from "../index";
 
@@ -42,150 +42,100 @@ interface FormProps {
     // text: string
 }
 interface State {
-    picture: any,
-    uploadStatus: string
-    selectedFile: any
+    badgeName: string,
+    badgeAudio: any,
+    badgeImage: any,
 };
 
 interface ThumbProp {
     selectedFile: any
 }
 
-function Previews(props) {
-    const [files, setFiles] = useState([]);
-    const { getRootProps, getInputProps } = useDropzone({
-        accept: 'image/*',
-        maxSize: 5242880,
-        multiple: false,
-        onDrop: (acceptedFiles: any) => {
-            setFiles(acceptedFiles.map(file => Object.assign(file, {
-                preview: URL.createObjectURL(file)
-            })));
-        }
-    });
-
-    const thumbs = files.map((file: any) => (
-        <div style={thumb} key={file.name}>
-            <div style={thumbInner}>
-                <img
-                    src={file.preview}
-                    style={img}
-                />
-            </div>
-        </div>
-    ));
-
-    useEffect(() => () => {
-        // Make sure to revoke the data uris to avoid memory leaks
-        files.forEach((file: any) => URL.revokeObjectURL(file.preview));
-    }, [files]);
-
-    return (
-        <section className="container">
-            <div {...getRootProps({ className: 'dropzone' })}>
-                <input {...getInputProps()} />
-                <p>Drag 'n' drop some files here, or click to select files</p>
-            </div>
-            <aside style={thumbsContainer}>
-                {console.log("Hooked files: ", files)}
-
-                {thumbs}
-            </aside>
-        </section>
-    );
-}
-
+//TODO make this into function with hook
 class TbForm extends React.Component<FormProps, State> {
     constructor(props: any) {
         super(props);
         this.state = {
-            picture: null,
-            uploadStatus: "Upload Image",
-            selectedFile: {}
+            badgeName: "",
+            badgeImage: null,
+            badgeAudio: null
         };
         this.onDrop = this.onDrop.bind(this);
-
+        this.submitBadge = this.submitBadge.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
     }
 
     //TODO use tooltip for additional information
-    onDrop(pic) {
+
+    //Name
+    handleNameChange = (event) => {
+        this.setState({ badgeName: event.target.value });
+    }
+
+    //Image
+    onDrop = (pic) => {
         console.log("Pic: ", pic[0])
         Object.assign(pic[0], {
             preview: URL.createObjectURL(pic[0])
         })
         this.setState({
-            picture: pic,
-            uploadStatus: "Image successfully uploaded"
+            badgeImage: pic,
         });
     }
 
-    fileChangedHandler = (event) => {
-        this.setState({ selectedFile: event.target.files[0] })
-
+    submitBadge = () => {
+        console.log("Submit Badge!", this.state);
     }
-
-    uploadHandler = () => {
-        console.log(this.state.selectedFile)
-    }
-
 
     render() {
         return (
             <div className="row tb-center">
-
-
+                <div>
+                    <h3>Input Name</h3>
+                    {/* TODO look into getting value without onChange. 
+                        Maybe form, maybe different react package 
+                        
+                        Also, look up how to not rerender entire page with just one state change
+                        Debug: Add console.log in Dropzone. Every text change rerenders it
+                        */}
+                    <input type="text" onChange={this.handleNameChange}></input>
+                </div>
                 {/* https://react-dropzone.js.org/ */}
                 {/* TODO get styling going on this */}
-
-
-                <Dropzone maxSize={5242880} multiple={false} accept='image/jpeg, image/png' onDrop={this.onDrop}>
-                    {({ getRootProps, getInputProps, acceptedFiles }) => {
-                        return (
-                            <div className="container">
-                                <div {...getRootProps({ className: 'dropzone' })}>
-                                    <input {...getInputProps()} />
-                                    <p>Drag 'n' drop some files here</p>
-                                    <button
-                                        type="button"
-                                    // onClick={openDialog}
-                                    >
-                                        Open File Dialog
-          </button>
-                                </div>
-                                <aside>
-                                    <h4>Files</h4>
-                                    {/* <ul>
-                                        {acceptedFiles.map((file: any) => (
-                                            <li key={file.path}>
-                                                {file.path} - {file.size} bytes
-              </li>
-                                        ))}
-                                    </ul> */}
-                                    <aside style={thumbsContainer}>
-                                        {console.log("Accepted files: ", acceptedFiles)}
-                                        {/* {acceptedFiles.map(file => Object.assign(file, {
-                                            preview: URL.createObjectURL(file)
-                                        }))} */}
-                                        {console.log("Selected Picture: ", this.state.picture)};
-                                        {/* <Thumb selectedFile={acceptedFiles}></Thumb> */}
-                                        {acceptedFiles.map((file: any) => (
-                                            <div style={thumb} key={file.name}>
-                                                <div style={thumbInner}>
-                                                    <img
-                                                        src={file.preview}
-                                                        style={img}
-                                                    />
+                <div>
+                    <h3>Upload Image</h3>
+                    <Dropzone maxSize={5242880} multiple={false} accept='image/jpeg, image/png' onDrop={this.onDrop}>
+                        {({ getRootProps, getInputProps, acceptedFiles }) => {
+                            return (
+                                <div className="container">
+                                    <div {...getRootProps({ className: 'dropzone' })}>
+                                        <input {...getInputProps()} />
+                                        <button type="button">Upload </button>
+                                    </div>
+                                    <aside>
+                                        <aside style={thumbsContainer}>
+                                            {acceptedFiles.map((file: any) => (
+                                                <div style={thumb} key={file.name}>
+                                                    <div style={thumbInner}>
+                                                        <img
+                                                            src={file.preview}
+                                                            style={img}
+                                                        />
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        ))}
+                                            ))}
+                                        </aside>
                                     </aside>
-                                </aside>
-                            </div>
-                        );
-                    }}
-                </Dropzone>
-                <Previews />
-            </div>
+                                </div>
+                            );
+                        }}
+                    </Dropzone>
+                </div>
+                <div>
+                    <h3>Record name</h3>
+                </div>
+                <Button disabled={!(this.state.badgeImage && this.state.badgeName)} variant="primary" onClick={this.submitBadge}>Submit</Button>
+            </div >
         )
     }
 };
