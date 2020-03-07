8acf85b99e9ec9506648c03bcec0a336a35dee84
Michielu Menning <mich.menning@gmail.com>
2020-03-06 20:25:50 -0600

    Add some styling to badge

--- src/components/RenderBadge/Badge.tsx
+++ src/components/RenderBadge/Badge.tsx
@@ -16,11 +16,12 @@ function Badge(badgeData, audioArr) {
     return (
         <div className="row">
             <div className=" col-md-6 offset-md-3 col-12">
-                <img className="tb-badge-image" src="https://i.pinimg.com/600x315/a6/0d/68/a60d685194a7fd984d08a595a0a99ae7.jpg" alt="Profile Image"></img>
-                {/* <img className="tb-badge-image" src="https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg" alt="Profile Image"></img> */}
+                {/* <img className="tb-badge-image" src="https://i.pinimg.com/600x315/a6/0d/68/a60d685194a7fd984d08a595a0a99ae7.jpg" alt="Profile Image"></img> */}
+                <img className="tb-badge-image" src="https://lh3.googleusercontent.com/proxy/Ge1uHw6CySqerYZhvj2Qrg2Pp704TbdLrodA2E1EuL6IIFvUknlrU5-4Z41DNhvfVI4iwTwxE9gEzOuWkh0w33HUNsC4yP8aNUIqXs2YXD-H5gZYvw" alt="Profile Image"></img>
                 <h3>{badgeData.name}</h3>
-                <Button disabled={!audio} variant="primary" onClick={pronounceName} block>Pronounce</Button>
-
+                <div className="tb-bottom-margin-10px">
+                    <Button disabled={!audio} variant="primary" onClick={pronounceName} block>Pronounce</Button>
+                </div>
             </div>
         </div>
     )
