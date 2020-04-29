f8d3cfb6e1aed38a57dd4f5bc6eebd67891aec85
Michielu Menning <mich.menning@gmail.com>
2020-04-28 20:08:27 -0500

    Add new page to nav

--- src/partials/TbNav/TbNav.tsx
+++ src/partials/TbNav/TbNav.tsx
@@ -12,9 +12,10 @@ const TbNav: React.FunctionComponent<NavProps> = () => {
                 <Navbar.Brand href="/">True Badge</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
+                    <Nav >
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/create">Create</Nav.Link>
+                        <Nav.Link href="/how">How it works</Nav.Link>
                         <Nav.Link href="/about">About</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
