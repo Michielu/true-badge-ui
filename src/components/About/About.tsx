6229674ff8830c197045e1413d1386d250f643bb
Michielu Menning <mich.menning@gmail.com>
2020-04-28 20:06:10 -0500

    Improve About page's content

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
-import { TbCard, TbJumbotron, TbButton } from '../../partials/';
+import { TbJumbotron, TbCTA } from '../../partials/';
 
 const About: React.FunctionComponent<{}> = () => {
-    const quote: string = `"Names are a wonderful thing. A person’s name is the greatest connection to their own identity. Being able to pronounce someone’s name correctly, -- as the person themselves wants it pronounced – is a sign of courtesy, respect, and care."`;
-    const quote2: string = `"We help you bridge the gap between face to names. We bundle your name, face, and how YOU pronounce your name onto one portable web page -- which you are given as a link -- so that you can share it with anyone."`;
+    const personal: string = "My name, Michielu(pronounced Mitchel), is an English name tweaked to make it easier for my Japanese relatives to pronounce. It has never been pronounced correctly without help. "
+    const personal2: string = "There are plenty of other people like me, people with ethnic or unique names, that I would like to help with this application."
     const pageTitle: string = "About";
-    const buttonProps = [{
+    const buttonProps = {
         value: "Create Badge",
         href: "/create"
-    }]
+    };
     return (
         <div>
             <TbJumbotron text={pageTitle}></TbJumbotron>
-            <TbCard texts={[quote, quote2]} />
+            <div className="row">
+                <div className="col-12 col-sm-4">
+                    <img className="tb-width-100 tb-box-shawdow tb-margin-5" src="/me.jpg" alt="Me"></img>
+                </div>
+                <div className="col-12 col-sm-8 tb-text-align-left">
+                    <p>{personal}</p>
+                    <p>{personal2}</p>
+                </div>
+            </div>
             <br />
             <div className="tb-center">
-                <TbButton buttons={buttonProps}></TbButton>
+                <TbCTA buttonProps={buttonProps} />
             </div>
             <br />
         </div>
