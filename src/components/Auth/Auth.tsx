a78485239166290475d744b7d0e6b6e1684227ca
Michielu Menning <mich.menning@gmail.com>
2020-06-01 19:48:25 -0500

    Toggle between login and sign up

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,16 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import TbLoader from "../../partials/TbLoader/TbLoader";
+
 import {
     useParams //TODO remove this import from this file
 } from "react-router-dom";
 
 
+// function renderLogin()
 
-function renderAuth(isBusy, isLogin) {
+function renderAuth(isBusy, isLogin, setLogin) {
+    if (isBusy) {
+        return <TbLoader show={isBusy} message="Loading..." />
+    }
+    if (isLogin) {
+        return (<div>
+            <h3>"Login"</h3>
+
+            <div>
+                <p>Login </p>
+                <small>Don't have an account? <p onClick={() => {
+                    console.log("hi")
+                    setLogin(!isLogin)
+                }}>Sign up</p></small>
+            </div>
+
+        </div>)
+    }
     return (
         <div>
-            <h3>{isLogin ? "Login" : "Register"}</h3>
+            <h3>"Register"</h3>
+            <div>
+                <p>Register </p>
+                <small>Already have an account? <p onClick={() => {
+                    console.log("hi")
+                    setLogin(!isLogin)
 
-            <p>Input </p>
+                }}>Login</p></small>
+                {/* <div>Already have an account? <a onClick={() => { setLogin(!isLogin) }} >Login</a></div> */}
+            </div>
         </div>
     )
 }
@@ -23,7 +50,9 @@ function Auth() {
     //Reseach if possible/good practice
     const [creds, setCreds] = useState();
 
-
+    // const toggleLogin = () => {
+    //     setLogin(!isLogin);
+    // }
 
     let url: any = useParams();
 
@@ -48,10 +77,20 @@ function Auth() {
     //     }
     // }, [isBusy, url]);
 
+    useEffect(() => {
+        async function setWait() {
+            await setTimeout(function () { alert("Hello"); }, 3000);
+            setBusy(false);
+        }
+        if (isBusy) {
+            setWait();
+        }
+    }, [isBusy])
+
     // We can use the `useParams` hook here to access
     // the dynamic pieces of the URL.
 
-    return renderAuth(isBusy, isLogin);
+    return renderAuth(isBusy, isLogin, setLogin);
 }
 
 export default Auth;
