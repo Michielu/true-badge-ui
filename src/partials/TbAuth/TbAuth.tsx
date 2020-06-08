66adab42998cee36511b312d76994e7d87821c55
Michielu Menning <mich.menning@gmail.com>
2020-06-08 18:12:36 -0500

    More small tbauth changes

--- src/partials/TbAuth/TbAuth.tsx
+++ src/partials/TbAuth/TbAuth.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Button, Form } from "react-bootstrap";
 
-function TbAuth() {
+function TbAuth(props) {
+    console.log("isLogin: ", props.isLogin)
     return (
         <div>
             <Form>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
+                    {/* <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
-                    </Form.Text>
+                    </Form.Text> */}
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
+                {props.isLogin ? null :
+                    <Form.Group controlId="formBasicPasswordRepeat">
+                        <Form.Label>Repeat Password</Form.Label>
+                        <Form.Control type="password" placeholder="Repeat Password" />
+                    </Form.Group>
+                }
                 <Form.Group controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
